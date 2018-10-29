0c19a20ed0a4b197a6614a3eb70ce38f0fd7286d
andriisukmanovskyi <andriisukmanovskyi@gmail.com>
2018-10-29 20:54:19 +0200

    added mobile capability type

--- src/main/java/com/epam/lab/uitls/init/driver/DriverInit.java
+++ src/main/java/com/epam/lab/uitls/init/driver/DriverInit.java
@@ -3,8 +3,10 @@ package com.epam.lab.uitls.init.driver;
 import io.appium.java_client.AppiumDriver;
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.Capabilities;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -14,11 +16,8 @@ import java.util.concurrent.TimeUnit;
 public class DriverInit {
     private static final Logger LOG = LogManager.getLogger(DriverInit.class);
 
-    private static final String DEVICE_NAME_CAPABILITY = "deviceName";
     private static final String DEVICE_NAME_CAPABILITY_VALUE = "MTP32443204437";
-    private static final String PLATFORM_NAME_CAPABILITY = "platformName";
     private static final String PLATFORM_NAME_CAPABILITY_VALUE = "Android";
-    private static final String PLATFORM_VERSION_CAPABILITY = "platformVersion";
     private static final String PLATFORM_VERSION_CAPABILITY_VALUE = "4.4.2";
     private static final String APP_PACKAGE_CAPABILITY = "appPackage";
     private static final String APP_PACKAGE_CAPABILITY_VALUE = "com.google.android.gm";
@@ -44,9 +43,9 @@ public class DriverInit {
     private static DesiredCapabilities getDesiredCapabilities() {
         LOG.info("Set desired capabilities");
         DesiredCapabilities capabilities = new DesiredCapabilities();
-        capabilities.setCapability(DEVICE_NAME_CAPABILITY, DEVICE_NAME_CAPABILITY_VALUE);
-        capabilities.setCapability(PLATFORM_NAME_CAPABILITY, PLATFORM_NAME_CAPABILITY_VALUE);
-        capabilities.setCapability(PLATFORM_VERSION_CAPABILITY, PLATFORM_VERSION_CAPABILITY_VALUE);
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, DEVICE_NAME_CAPABILITY_VALUE);
+        capabilities.setCapability(MobileCapabilityType.APPLICATION_NAME, PLATFORM_NAME_CAPABILITY_VALUE);
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, PLATFORM_VERSION_CAPABILITY_VALUE);
         capabilities.setCapability(APP_PACKAGE_CAPABILITY, APP_PACKAGE_CAPABILITY_VALUE);
         capabilities.setCapability(APP_ACTIVITY_CAPABILITY, APP_ACTIVITY_CAPABILITY_VALUE);
         return capabilities;
